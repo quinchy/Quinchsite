e1177625411d37feb02480d72b76b1d562837981
Cyril James De Guzman <deguzmancyriljames.dev@gmail.com>
2026-02-12 00:54:17 +0800

    chore: add mobile view for footer, experiences, hero, and projects

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-between w-full mx-auto pt-20 pb-4 text-sm">
+    <footer className="flex flex-col gap-1 lg:flex-row items-center justify-between w-full mx-auto pt-20 pb-4 text-xs lg:text-sm">
       <p>Made with Next.js</p>
-      <p>© 2026 Cyril James De Guzman. All Rights Reserved.</p>
+      <p className="text-center lg:text-end">
+        © 2026 Cyril James De Guzman. All Rights Reserved.
+      </p>
     </footer>
   );
 }
