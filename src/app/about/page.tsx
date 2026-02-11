ac61c2b5c3c739e39bf0a8c93527c4ef3b3778be
Cyril James De Guzman <deguzmancyriljames.dev@gmail.com>
2026-02-11 21:03:08 +0800

    chore: reimport sections on about and projects page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,6 @@ export default function About() {
     <main className="h-[calc(100dvh-38px)] pt-8 translate-y-9.5 overflow-y-auto overflow-x-hidden border-x border-b border-border">
       <div className="max-w-4xl mx-auto flex flex-col gap-16 p-4">
         <Hero />
-        <About />
         <Experiences />
         <Footer />
       </div>
